b65c511a0474e92b50d01dc5e0397404b7e1ff8d
ninjaa <alkhudrisaid83@gmail.com>
2020-09-17 10:18:32 +0700

    first commit

--- remed2.js
+++ remed2.js
@@ -1,7 +1,7 @@
 var nilaiSantri = [100, 20, 35, 70, 99, 80, 100, 97, 84];
 var total = 0;
 
-for (i in nilaiSantri) {
+for (i = 0; i < nilaiSantri.length; i++) {
   total += nilaiSantri[i];
 }
-document.write(total / nilaiSantri[i]);
+document.write(total / nilaiSantri.length);
